579769916fd4884162b1dc4b3edb1de5ccfe529e
raj u <r@Rs-MacBook-Air.local>
2020-07-24 15:11:51 -0700

    sprint mvp

--- ls8/ls8.py
+++ ls8/ls8.py
@@ -1,11 +1,23 @@
-#!/usr/bin/env python3
-
 """Main."""
 
+import os
 import sys
 from cpu import *
+'''
+Usage:
+python(3) ls8.py call -> loads ../examples/call.ls8 into cpu
+To load new programs, add program file to /examples folder
+'''
+
+current_dir = os.getcwd() + '\examples\\'
+
+
+if sys.argv[1][-4] == '.':
+    command = current_dir + sys.argv[1]
+else:
+    command = current_dir + sys.argv[1] + '.ls8'
 
 cpu = CPU()
 
-cpu.load()
+cpu.load(command)
 cpu.run()
